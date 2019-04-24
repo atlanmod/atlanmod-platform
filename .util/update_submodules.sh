6dda8511fa932734f985aaa8f1d201e1dc63aefa
Josselin Enet <enet.josselin@gmail.com>
2019-04-24 14:50:38 +0200

    Fixed update_submodules script
    The script is now working correctly even if the submodules are more than
    one directory away from the root of the project.

--- .util/update_submodules.sh
+++ .util/update_submodules.sh
@@ -2,14 +2,20 @@
 
 # Updates a single submodule
 update_sub() {
+    root_dist=$(echo $1 | grep -o "/" | wc -l)
     cd $1
     git submodule update --init
-    cd ..
+    i=0
+    while [ $i -le $root_dist ]
+    do
+        cd ..
+        ((i++))
+    done
 }
 
 main() {
-    submodules=$(cat .gitmodules | grep "path = " | cut -c9-)
-    for sub in $submodules
+    submods=$(grep "path = " .gitmodules | cut -c9-)
+    for sub in $submods
     do
         update_sub $sub
     done
