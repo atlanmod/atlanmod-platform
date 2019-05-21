c9ba0617e39adf9ee538b498fea6036169bb60e0
Josselin Enet <enet.josselin@gmail.com>
2019-05-21 14:44:49 +0200

    Added Modisco as a submodule
    Modisco should now be correctly configured as a submodule.

--- modisco/org.eclipse.modisco.jee.jsp.discoverer/src/org/eclipse/modisco/jee/jsp/discoverer/JSPDiscoveryConstants.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.modisco.jee.jsp.discoverer;
-
-public final class JSPDiscoveryConstants {
-	private JSPDiscoveryConstants() {
-		// constants
-	}
-
-	public static final String JSP_MODEL_FILE_SUFFIX = "_jsp.xmi"; //$NON-NLS-1$
-}
